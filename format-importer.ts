e87b0b5ff6f404bb589b9bdd0f7094dd1a88a351
Erica Xu <iam@ericaxu.com>
2023-07-18 12:15:08 -0400

    Refactor

---
+++ format-importer.ts
@@ -0,0 +1,12 @@
+import { App } from "obsidian";
+import { ImportResult } from "yarle/interfaces";
+
+export abstract class FormatImporter {
+	app: App;
+
+	constructor(app: App) {
+		this.app = app;
+	}
+
+        abstract import(filePaths: string[], outputFolder: string): Promise<ImportResult>;
+}
