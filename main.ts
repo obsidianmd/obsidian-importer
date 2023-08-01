0856fbd6140eeb0bb37ac995ab746c40539cbf47
lishid <lishid@gmail.com>
2023-08-01 13:27:30 -0400

    Simplify ImporterModal.

--- main.ts
+++ main.ts
@@ -61,14 +61,14 @@ export class ImporterModal extends Modal {
 		this.plugin = plugin;
 		this.titleEl.setText('Import data into Obsidian');
 
-		let keys = Object.keys(this.importers);
+		let keys = Object.keys(plugin.importers);
 		if (keys.length > 0) {
 			this.updateContent(keys[0]);
 		}
 	}
 
 	updateContent(selectedId: string) {
-		const { contentEl, importers } = this;
+		const { contentEl, plugin: { importers } } = this;
 		contentEl.empty();
 
 		new Setting(contentEl)
@@ -104,10 +104,6 @@ export class ImporterModal extends Modal {
 				});
 			});
 		}
-	}
-
-	get importers() {
-		return this.plugin.importers;
 	}
 
 	onClose() {
