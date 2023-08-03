0ead1705908620505d1f1507c8d1bac807bdf7a7
William So <polyipseity@gmail.com>
2023-08-04 07:48:56 +0800

    Minor tweaks
    Signed-off-by: William So <polyipseity@gmail.com>

--- formats/html.ts
+++ formats/html.ts
@@ -81,14 +81,14 @@ export class HtmlImporter extends FormatImporter {
 			errors: [],
 		};
 
-		const executor = new PromiseExecutor(1);
+		const executor = new PromiseExecutor(2);
 		const processed = await Promise.allSettled(files.map(file => executor.run(() => this.processFile(result, folder, file))));
 		result.errors = result.errors.concat(processed
 			.filter((p): p is typeof p & { status: "rejected" } => p.status === "rejected")
 			.map(({ reason }) => reason));
 
 		this.showResult(result);
-		console.error(...result.errors);
+		console.error(result.errors);
 	}
 
 	async processFile(result: ImportResult & { errors: unknown[] }, folder: TFolder, file: PickedFile) {
