8db7fb61bbe6cdfec5271e48dffbcfd1bd9b3d84
Arthur Tyukayev <github_arthur@tyukayev.com>
2023-08-02 22:52:59 -0700

    Fixed main.ts after rebase

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
+import { App, Modal, Plugin, Setting } from 'obsidian';
 import { FormatImporter } from './format-importer';
 import { EvernoteEnexImporter } from './formats/evernote-enex';
 import { HtmlImporter } from './formats/html';
 import { Bear2bkImporter } from 'formats/bear-bear2bk';
-import { App, Modal, Plugin, Setting } from 'obsidian';
 
 declare global {
 	interface Window {
@@ -35,14 +35,14 @@ export default class ImporterPlugin extends Plugin {
 				importer: HtmlImporter,
 			},
       'bear': {
-        name: 'Bear (.bear2bk)',
+        name: 'Bear (.bearbk)',
         importer: Bear2bkImporter,
       }
 		};
 
 		this.addRibbonIcon('lucide-import', 'Open Importer', () => {
 			new ImporterModal(this.app, this).open();
-		})
+		});
 
 		this.addCommand({
 			id: 'open-modal',
@@ -66,14 +66,14 @@ export class ImporterModal extends Modal {
 		this.plugin = plugin;
 		this.titleEl.setText('Import data into Obsidian');
 
-		let keys = Object.keys(this.importers);
+		let keys = Object.keys(plugin.importers);
 		if (keys.length > 0) {
 			this.updateContent(keys[0]);
 		}
 	}
 
 	updateContent(selectedId: string) {
-		const { contentEl, importers } = this;
+		const { contentEl, plugin: { importers } } = this;
 		contentEl.empty();
 
 		new Setting(contentEl)
@@ -109,10 +109,6 @@ export class ImporterModal extends Modal {
 				});
 			});
 		}
-	}
-
-	get importers() {
-		return this.plugin.importers;
 	}
 
 	onClose() {
