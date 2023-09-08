9571ec6bd4ff24e5376f2c26b131fff66d730d91
Steph Ango <10565871+kepano@users.noreply.github.com>
2023-09-08 09:56:02 -0700

    Roam: Simplify description for attachments

--- src/formats/roam-json.ts
+++ src/formats/roam-json.ts
@@ -31,7 +31,7 @@ export class RoamJSONImporter extends FormatImporter {
 
 		new Setting(this.modal.contentEl)
 			.setName('Download all attachments')
-			.setDesc('If enabled, attachments previously uploaded to Roam will be downloaded to your attachments folder. WARNING: this can take a large amount of space.')
+			.setDesc('If enabled, all attachments uploaded to Roam will be downloaded to your attachments folder.')
 			.addToggle(toggle => {
 				toggle.setValue(this.downloadAttachments);
 				toggle.onChange(async (value) => {
