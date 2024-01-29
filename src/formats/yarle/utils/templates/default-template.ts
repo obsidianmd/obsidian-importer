c70d86d9b20e315e3b6ca3a3027b016bb3ac6600
Akos Balasko <akos0215@gmail.com>
2024-01-29 21:56:40 +0100

    #200 - source url in properties

--- src/formats/yarle/utils/templates/default-template.ts
+++ src/formats/yarle/utils/templates/default-template.ts
@@ -1,5 +1,7 @@
-const tagBlock = '{tags-array-block}---\ntags: {tags-array}\n---\n{end-tags-array-block}';
+const frontmatterDelimiter = '---\n';
+const sourceBlock = '{source-url-block}\nsource: {source-url}\n\n{end-source-url-block}';
+const tagBlock = '{tags-array-block}\ntags: {tags-array}\n\n{end-tags-array-block}';
 const titleBlock = '{title-block}# {title}\n\n{end-title-block}';
 const contentBlock = '{content-block}{content}{end-content-block}\n';
 
-export const defaultTemplate = tagBlock + titleBlock + contentBlock;
+export const defaultTemplate = frontmatterDelimiter + tagBlock + sourceBlock +frontmatterDelimiter+ titleBlock + contentBlock;
