7c1e0fde3c2c74aa70bb060559ef3d738a5d26f4
Akos Balasko <akos0215@gmail.com>
2024-01-30 18:51:20 +0100

    title removed as H1

--- src/formats/yarle/utils/templates/default-template.ts
+++ src/formats/yarle/utils/templates/default-template.ts
@@ -1,7 +1,6 @@
 const frontmatterDelimiter = '---\n';
 const sourceBlock = '{source-url-block}source: {source-url}{end-source-url-block}\n';
 const tagBlock = '{tags-yaml-list-block}\ntags: {tags-yaml-list}\n\n{end-tags-yaml-list-block}';
-const titleBlock = '{title-block}# {title}\n\n{end-title-block}';
 const contentBlock = '{content-block}{content}{end-content-block}\n';
 
-export const defaultTemplate = frontmatterDelimiter + tagBlock + sourceBlock +frontmatterDelimiter+ titleBlock + contentBlock;
+export const defaultTemplate = frontmatterDelimiter + tagBlock + sourceBlock +frontmatterDelimiter + contentBlock;
