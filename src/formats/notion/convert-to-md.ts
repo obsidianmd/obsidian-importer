757ae19284107ba2cd0530bb965a84e20be4e13c
felciabatta <felcia.dp@gmail.com>
2024-12-08 01:47:43 +0000

    fix issue with linebreaks within inline math mode

--- src/formats/notion/convert-to-md.ts
+++ src/formats/notion/convert-to-md.ts
@@ -235,21 +235,22 @@ function fixEquations(body: HTMLElement) {
 	for (const figEqn of figEqnEls) {
 		const annotation = figEqn.find('annotation');
 		if (!annotation) continue;
-		figEqn.replaceWith(`$$${trimMath(annotation.textContent)}$$`);
+		figEqn.replaceWith(`$$${formatMath(annotation.textContent)}$$`);
 	}
 	// Inline Equations
 	const spanEqnEls = body.findAll('span.notion-text-equation-token');
 	for (const spanEqn of spanEqnEls) {
 		const annotation = spanEqn.find('annotation');
 		if (!annotation) continue;
-		spanEqn.replaceWith(`$${trimMath(annotation.textContent)}$`);
+		spanEqn.replaceWith(`$${formatMath(annotation.textContent, true)}$`);
 	}
 }
 
-function trimMath(math: string | null | undefined): string {
-	// Trim trailing spaces & newlines in LaTeX math experessions.
+function formatMath(math: string | null | undefined, inline: boolean=false): string {
+	// Trim lead/trailing space in LaTeX math experessions.
+	// Removes empty lines, which can cause equations to break.
 	let regex = new RegExp(/^(?:[\s\r\n]|\\\\|\\\s)*(.*?)[\s\r\n\\]*$/, 's');
-	return math?.replace(regex, '$1') ?? '';
+	return math?.replace(regex, '$1').replace(/[\r\n]+/g, (inline ? ' ' : '\n')) ?? '';
 }
 
 function stripToSentence(paragraph: string) {
