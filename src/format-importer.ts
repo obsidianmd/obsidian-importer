323b676b5e573d0fe753f4c9356e60682ba9a871
farnaby <farnaby@web.de>
2025-11-04 00:29:36 +0100

    Tomboy / GNote importer (#445)
    ---------
    
    Co-authored-by: Daniel an der Brügge <daniel.anderbruegge@four-audio.com>

--- src/format-importer.ts
+++ src/format-importer.ts
@@ -37,10 +37,10 @@ export abstract class FormatImporter {
 		this.modal.plugin.registerAuthCallback(callback);
 	}
 
-	addFileChooserSetting(name: string, extensions: string[], allowMultiple: boolean = false) {
+	addFileChooserSetting(name: string, extensions: string[], allowMultiple: boolean = false, description?: string, defaultPath?: string) {
 		let fileLocationSetting = new Setting(this.modal.contentEl)
 			.setName('Files to import')
-			.setDesc('Pick the files that you want to import.')
+			.setDesc(description || 'Pick the files that you want to import.')
 			.addButton(button => button
 				.setButtonText(allowMultiple ? 'Choose files' : 'Choose file')
 				.onClick(async () => {
@@ -52,6 +52,7 @@ export abstract class FormatImporter {
 						let filePaths: string[] = window.electron.remote.dialog.showOpenDialogSync({
 							title: 'Pick files to import', properties,
 							filters: [{ name, extensions }],
+							defaultPath: defaultPath || undefined,
 						});
 
 						if (filePaths && filePaths.length > 0) {
@@ -84,6 +85,7 @@ export abstract class FormatImporter {
 						let filePaths: string[] = window.electron.remote.dialog.showOpenDialogSync({
 							title: 'Pick folders to import',
 							properties: ['openDirectory', 'multiSelections', 'dontAddToRecent'],
+							defaultPath: defaultPath || undefined,
 						});
 
 						if (filePaths && filePaths.length > 0) {
@@ -243,4 +245,3 @@ export abstract class FormatImporter {
 		return await this.app.fileManager.createNewMarkdownFile(folder, sanitizedName, content);
 	}
 }
-
