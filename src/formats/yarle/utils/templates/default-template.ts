dbaf19b8f9a473368486b27e568c2f50956dd593
Zarino Zappia <mail@zarino.co.uk>
2025-12-20 20:40:45 +0000

    Add created and updated dates to ENEX import
    If the ENEX file contains created and updated dates (Evernote includes
    these in exports by default, but they can be disabled) they will be
    added as `created` and `updated` properties on the imported notes.

--- src/formats/yarle/utils/templates/default-template.ts
+++ src/formats/yarle/utils/templates/default-template.ts
@@ -1,6 +1,8 @@
 const frontmatterDelimiter = '---\n';
+const createdAtBlock = '{created-at-block}created: {created-at}{end-created-at-block}\n';
+const updatedAtBlock = '{updated-at-block}updated: {updated-at}{end-updated-at-block}\n';
 const sourceBlock = '{source-url-block}source: {source-url}{end-source-url-block}\n';
 const tagBlock = '{tags-yaml-list-block}\ntags: {tags-yaml-list}\n\n{end-tags-yaml-list-block}';
 const contentBlock = '{content-block}{content}{end-content-block}\n';
 
-export const defaultTemplate = frontmatterDelimiter + tagBlock + sourceBlock +frontmatterDelimiter + contentBlock;
+export const defaultTemplate = frontmatterDelimiter + tagBlock + createdAtBlock + updatedAtBlock + sourceBlock + frontmatterDelimiter + contentBlock;
