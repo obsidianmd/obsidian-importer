0a2aef6f43bca7e6db58d662cb86ced6d9ddc3fd
Zarino Zappia <mail@zarino.co.uk>
2025-12-20 21:19:19 +0000

    Add unescaped title import to ENEX import

--- src/formats/yarle/utils/templates/default-template.ts
+++ src/formats/yarle/utils/templates/default-template.ts
@@ -1,8 +1,9 @@
 const frontmatterDelimiter = '---\n';
+const titleBlock = '{title-block}title: {title}{end-title-block}\n';
 const createdAtBlock = '{created-at-block}created: {created-at}{end-created-at-block}\n';
 const updatedAtBlock = '{updated-at-block}updated: {updated-at}{end-updated-at-block}\n';
 const sourceBlock = '{source-url-block}source: {source-url}{end-source-url-block}\n';
 const tagBlock = '{tags-yaml-list-block}\ntags: {tags-yaml-list}\n\n{end-tags-yaml-list-block}';
 const contentBlock = '{content-block}{content}{end-content-block}\n';
 
-export const defaultTemplate = frontmatterDelimiter + tagBlock + createdAtBlock + updatedAtBlock + sourceBlock + frontmatterDelimiter + contentBlock;
+export const defaultTemplate = frontmatterDelimiter + titleBlock + tagBlock + createdAtBlock + updatedAtBlock + sourceBlock + frontmatterDelimiter + contentBlock;
