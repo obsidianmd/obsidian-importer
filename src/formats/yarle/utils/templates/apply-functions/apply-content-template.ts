70c4caf01cdced22c0e55cd393408edc261d610d
池下 克彦 <kik@nvstl.com>
2026-02-07 18:31:40 +0900

    Enable typescript-eslint recommended rules
    - @typescript-eslint/no-unsafe-function-type
    - @typescript-eslint/no-this-alias
    - @typescript-eslint/prefer-as-const

--- src/formats/yarle/utils/templates/apply-functions/apply-content-template.ts
+++ src/formats/yarle/utils/templates/apply-functions/apply-content-template.ts
@@ -1,10 +1,11 @@
 import { NoteData } from '../../../models/NoteData';
 import * as P from '../placeholders/content-placeholders';
+import { CheckFunction } from '../template-settings';
 
 import { applyTemplateOnBlock } from './apply-template-on-block';
 import { getTemplateBlockSettings } from './get-templateblock-settings';
 
-export const applyContentTemplate = (noteData: NoteData, inputText: string, check: Function): string => {
+export const applyContentTemplate = (noteData: NoteData, inputText: string, check: CheckFunction): string => {
 	const contentTemplateSettings = getTemplateBlockSettings(inputText, check, P, noteData.content);
 
 	return applyTemplateOnBlock(contentTemplateSettings);
