999f23392d9c4c31fc37ae4a476285ce515f2f05
fnnikita <nikitalin126@gmail.com>
2026-06-21 21:16:13 -0700

    Fix OneNote progress for skipped pages

--- src/formats/onenote.ts
+++ src/formats/onenote.ts
@@ -458,6 +458,7 @@ export class OneNoteImporter extends FormatImporter {
 
 				if (!this.importPreviouslyImported && page.id && previouslyImported.has(page.id)) {
 					progress.reportSkipped(page.title, 'it was previously imported');
+					progress.reportProgress(++progressCurrent, progressTotal);
 					continue;
 				}
 
